3df3fd1ad5d2436175c7ec54db4c9f6cc08dac78
Hachounet <hachounet.github@gmx.fr>
2024-09-23 14:16:06 +0200

    FEAT Auth controller && router

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,66 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const asyncHandler = require("express-async-handler");
-const { body, validationResult } = require("express-validator");
+const { validationResult } = require("express-validator");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const errorMessage = require("../errorMessages");
+const validateSignUp = require("../helpers/validateSignUp");
 
 exports.postLoginPage = asyncHandler(async (req, res, next) => {
-  console.log("WIP");
+  const errors = [];
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: req.body.email,
+    },
+  });
+
+  if (!user) {
+    errors.push({ msg: errorMessage.INVALID_EMAIL });
+  }
+
+  let isPasswordValid = true;
+  if (user) {
+    isPasswordValid = await bcryptjs.compare(req.body.pw, user.hash);
+    if (!isPasswordValid) {
+      errors.push({ msg: errorMessage.INVALID_PASSWORD });
+    }
+  }
+
+  if (errors.length > 0) {
+    return res.status(400).json({ errors });
+  }
+
+  const accessToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+
+  return res.status(200).json({ message: "User logged in", accessToken });
 });
 
-exports.postLogoutPage = asyncHandler(async (req, res, next) => {
-  console.log("WIP");
-});
+exports.postSignupPage = [
+  validateSignUp,
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-exports.postSignupPage = asyncHandler(async (req, res, next) => {
-  console.log("WIP");
-});
+    bcryptjs.hash(req.body.pw, 10, async (err, hashedPassword) => {
+      if (err) {
+        return next(err);
+      }
+      await prisma.user.create({
+        data: {
+          email: req.body.email,
+          pseudo: req.body.pseudo,
+          hash: hashedPassword,
+        },
+      });
+      return res
+        .status(200)
+        .json({ success: true, message: "Account successfully created !" });
+    });
+  }),
+];
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,14 +3,11 @@ const passport = require("passport");
 
 const {
   postLoginPage,
-  postLogoutPage,
   postSignUpPage,
 } = require("../controllers/authController");
 
 const authRouter = Router();
 
 authRouter.post("/login", postLoginPage);
-
-authRouter.post("/logout", postLogoutPage);
 
 authRouter.post("/signup", postSignUpPage);
