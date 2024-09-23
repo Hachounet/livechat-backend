a9aba0497925279b45c8d08b4533e7c01e4ab6fd
Hachounet <hachounet.github@gmx.fr>
2024-09-23 14:33:45 +0200

    DB WIP Seed.js script

---
+++ prisma/seed.js
@@ -0,0 +1,76 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+async function main() {
+  const usersData = [
+    {
+      email: "fakealice@fakemail.com",
+      pseudo: "Fake_Alice",
+      birthdate: "2010-01-01",
+    },
+    {
+      email: "fakebob@fakeemail.com",
+      pseudo: "Fake_Bob",
+      birthdate: "2009-01-01",
+    },
+    {
+      email: "faketarzan@fakeemail.com",
+      pseudo: "Fake_Tarzan",
+      birthdate: "2008-01-01",
+    },
+    {
+      email: "fakejane@fakeemail.com",
+      pseudo: "Fake_Jane",
+      birthdate: "2007-01-01",
+    },
+    {
+      email: "fakebuzz@fakeemail.com",
+      pseudo: "Fake_Buzz",
+      birthdate: "2006-01-01",
+    },
+    {
+      email: "fakealex@fakeemail.com",
+      pseudo: "Fake_Alex",
+      birthdate: "2005-01-01",
+    },
+  ];
+
+  const createdUsers = await Promise.all(
+    usersData.map((user) =>
+      prisma.user.create({
+        data: {
+          email: user.email,
+          pseudo: user.pseudo,
+          hash: "fakehash1234",
+          birthdate: DateTime.fromISO(user.birthdate).toISO(),
+          fakeAccount: true,
+        },
+      }),
+    ),
+  );
+
+  // Create friendship between them
+  for (let i = 0; i < createdUsers.length; i++) {
+    for (let j = 0; j < createdUsers.length; j++) {
+      if (i !== j) {
+        await prisma.friendRequest.create({
+          data: {
+            senderId: createdUsers[i].id,
+            receiverId: createdUsers[j].id,
+            status: "ACCEPTED",
+          },
+        });
+      }
+    }
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
