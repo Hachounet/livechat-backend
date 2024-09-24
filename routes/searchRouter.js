742b5261ace3c286f58bd2912b77d19a7be9ffb5
Hachounet <hachounet.github@gmx.fr>
2024-09-24 15:28:39 +0200

    FIX Import search router

--- routes/searchRouter.js
+++ routes/searchRouter.js
@@ -1,6 +1,11 @@
 const { Router } = require("express");
 const { authenticateJWT } = require("../auth/passport");
 
+const {
+  getUsersListPage,
+  getGroupListPage,
+} = require("../controllers/searchController");
+
 const searchRouter = Router();
 
 searchRouter.get("/users?query=xyz", authenticateJWT, getUsersListPage);
