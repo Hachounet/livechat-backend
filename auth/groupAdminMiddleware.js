a2c0a9ed15deb27efc970b30e3d1a7b212661a5a
Hachounet <hachounet.github@gmx.fr>
2024-09-24 10:45:32 +0200

    FEAT Middleware isAdmin

---
+++ auth/groupAdminMiddleware.js
@@ -0,0 +1,28 @@
+const asyncHandler = require("express-async-handler");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+exports.isAdmin = asyncHandler(async (req, res, next) => {
+  const userId = req.user.id;
+  const groupId = req.params.groupId;
+
+  const group = await prisma.group.findUnique({
+    where: {
+      id: req.groupId,
+    },
+  });
+
+  if (!group) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Group not found." });
+  }
+
+  if (group.ownerId === userId) {
+    return next();
+  } else {
+    return res
+      .status(401)
+      .json({ success: false, message: "Operation not allowed." });
+  }
+});
