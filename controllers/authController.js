874a7fc3ee061205c6d20c2e53a728695ba5e956
Hachounet <hachounet.github@gmx.fr>
2024-09-25 10:11:21 +0200

    FIX authController import switch with internal func

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,74 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const asyncHandler = require("express-async-handler");
-const { validationResult } = require("express-validator");
+const { validationResult, body } = require("express-validator");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const errorMessage = require("../errorMessages");
-const validateSignUp = require("../helpers/validateSignUp");
+const errorMessages = require("../errorMessages");
+
+function checkForJSAttack(value) {
+  const regex = /<script.*?>.*?<\/script>/i;
+  if (regex.test(value)) {
+    throw new Error("Username cannot contain JavaScript code.");
+  }
+  const invalidChars = /[^a-zA-Z0-9-_]/;
+  if (invalidChars.test(value)) {
+    throw new Error(
+      "Username can only contain alphanumeric characters, dashes and underscores.",
+    );
+  }
+  return true;
+}
+
+function validateBirthdate(value) {
+  const birthdate = DateTime.fromISO(value);
+  const today = DateTime.now();
+  const age = today.diff(birthdate, "years").years;
+
+  if (!birthdate.isValid) {
+    throw new Error(errorMessages.INVALID_DATE);
+  }
+  if (age < 13) {
+    throw new Error(errorMessages.UNDERAGE);
+  }
+  return true;
+}
+
+const validateSignUp = [
+  body("pseudo")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage(`Pseudo ${errorMessages.NOT_EMPTY}`)
+    .isLength({ min: 3, max: 20 })
+    .withMessage(`Pseudo ${errorMessages.LENGTH_3_TO_20}`)
+    .custom(checkForJSAttack),
+  body("email")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage(`Email ${errorMessages.NOT_EMPTY}`)
+    .normalizeEmail()
+    .isEmail()
+    .withMessage(`${errorMessages.MAIL_FORMAT}`),
+  body("birthdate")
+    .notEmpty()
+    .withMessage(`Birthdate ${errorMessages.NOT_EMPTY}`)
+    .custom(validateBirthdate),
+  body("pw")
+    .notEmpty()
+    .withMessage(`Password ${errorMessages.NOT_EMPTY}`)
+    .trim()
+    .isLength({ min: 6 })
+    .withMessage(`Password ${errorMessages.LENGTH_6}`)
+    .custom(checkForJSAttack)
+    .custom((value, { req }) => {
+      if (value !== req.body.confpw) {
+        throw new Error(errorMessages.PASSWORDS_DO_NOT_MATCH);
+      }
+      return true;
+    }),
+];
 
 exports.postLoginPage = asyncHandler(async (req, res, next) => {
   const errors = [];
@@ -39,7 +102,7 @@ exports.postLoginPage = asyncHandler(async (req, res, next) => {
   return res.status(200).json({ message: "User logged in", accessToken });
 });
 
-exports.postSignupPage = [
+exports.postSignUpPage = [
   validateSignUp,
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
